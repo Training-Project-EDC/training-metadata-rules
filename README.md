537d05f4f13ba127a456b11bd34247ab6b0f3a44
Moses Chawawa <moseschawawa@gmail.com>
2021-07-27 11:46:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# esr21-metadata-rules
+esr21 Subject Metadata Rules
+# training-metadata-rules
---
+++ shell.sh
@@ -0,0 +1,7 @@
+echo "# training-metadata-rules" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Training-Project-EDC/training-metadata-rules.git
+git push -u origin main
